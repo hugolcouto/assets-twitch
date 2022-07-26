2f377e6a4565cf042c9aa6409d08848532b17c16
Hugo Couto <hugo.couto@webhub.com.br>
2022-07-26 08:55:39 -0300

    Ajuste no local do arquivo de agenda

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	$.getJSON('../agenda.json', function (data) {
+	$.getJSON('./agenda.json', function (data) {
 		$('.schedule-content').html(
 			data.agenda.map(function (item) {
 				return `<li>${item.week}: <span class="${item.vacancy && 'vacancy'}">${item.title}</span></li>`
